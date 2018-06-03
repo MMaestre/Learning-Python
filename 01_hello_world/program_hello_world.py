516c3508c0497edc7d292c139eb8411da933dd50
MMaestre <airam.maestre@gmail.com>
2018-06-03 20:27:44 +0200

    Hello World
    #01 Hello World App
    Print, ask and answer

---
+++ 01_hello_world/program_hello_world.py
@@ -0,0 +1,10 @@
+print('--------------------------------')
+print('          HELLO WORLD APP')
+print('--------------------------------')
+print()
+
+print('¡Hola!')
+name=input('¿Cómo te llamas? ')
+greeting='Encantado de conocerte '+name+' .'
+
+print(greeting)
